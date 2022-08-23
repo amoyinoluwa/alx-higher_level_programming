f7a84e1755627d63c43662d972eb3537ce4de0e7
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-08-23 11:46:58 -0700

    fixed syntax warning

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for a in range(0, 9):
     for b in range(1 + a, 10):
         print("{:d}{:d}".format(a, b), end="")
-        if (a + b) is not 17:
+        if (a + b) != 17:
             print(",", end=" ")
 print()
