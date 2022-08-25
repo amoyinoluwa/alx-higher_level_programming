0f25dda7cc521cae574e90276f6b6eaa6ba60b36
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-08-25 10:52:28 -0700

    updated requirements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,12 @@
 if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
-    print("{} arguments".format(argc - 1))
-    if argc > 1:
-        for i in range(1, argc):
-            print("{}: {}".format(i , sys.argv[i]))
+    if argc == 1:
+        print("0 arguments.")
+    if argc == 2:
+        print("1 argument:")
+    if argc > 2:
+        print("{} arguments:".format(argc - 1))
+    for i in range(1, argc):
+        print("{}: {}".format(i , sys.argv[i]))
 
