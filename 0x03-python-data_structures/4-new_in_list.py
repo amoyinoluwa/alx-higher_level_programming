892bb12fb9e8c910bd348a17f87759f59b91b6ac
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-08-30 05:20:54 -0700

    added copy() method

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    b = my_list
+    b = my_list.copy()
     if idx in range(0, len(my_list)):
         b[idx] = element
         return b
-    return b
+    return my_list
