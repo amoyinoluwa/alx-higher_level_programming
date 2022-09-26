8123b281ffa2b9c8a4ced87bcf77d2b280eef0f9
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-09-26 14:04:17 -0700

    files added

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Empty class BaseGeometry"""
+
+class BaseGeometry:
+    def __init__(self):
+        pass
+    def area(self):
+        raise Exception("area() is not implemented")
