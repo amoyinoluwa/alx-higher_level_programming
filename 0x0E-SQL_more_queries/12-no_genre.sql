06b86c7f0e5902268ebb8e703cf25c77ed123331
amoyinoluwa <amoyinoluwa24@gmail.com>
2022-11-03 15:31:01 -0700

    --

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
--- script that lists all shows contained in the database
+-- Select onlu null values
 SELECT tv.title, tvs.genre_id
 FROM tv_shows AS tv
 LEFT JOIN tv_show_genres AS tvs
 ON tvs.show_id = tv.id
+WHERE tvs.genre_id IS NULL
 ORDER BY tv.title, tvs.genre_id;
